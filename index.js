997fd46c6248f9090852c049fe57e5f448dbd64d
Eric Bishard <e.bishard@gmail.com>
2018-12-29 17:50:11 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 80))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('It\'s a React State of Mind!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running locally at:" + app.get('port'))
+})
