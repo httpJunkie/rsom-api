80c31319f8229784b3ebadeaa8c7b41931bb6c9f
Eric Bishard <e.bishard@gmail.com>
2018-12-30 16:50:19 -0800

    Simplify index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
 const path = require('path');
-const app = express()
+const app = express();
 
-
-app.use(express.static(path.join(__dirname,'../rsom-client/dist')))
+app.use(express.static(path.join(__dirname,'../rsom-client/dist')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../rsom-client/dist', 'index.html'));
 });
 
-
-app.listen(process.env.PORT || 80), function() {
-  console.log("Node app is running")
-};
+app.listen(process.env.PORT || 80);
